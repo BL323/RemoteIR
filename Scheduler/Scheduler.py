8229d6f0236ce5d654b43f61b3965e582f219634
BLourence <ben@lourence.com>
2015-05-04 13:23:04 +0000

    Runs schedule checker as seperate process

--- Scheduler/Scheduler.py
+++ Scheduler/Scheduler.py
@@ -1,13 +1,14 @@
+import time
+import datetime
 import os
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, and_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '../app.db')
-
-print SQLALCHEMY_DATABASE_URI
 
 class Task(declarative_base()):
 	__tablename__ = "task"
@@ -19,9 +20,23 @@ class Task(declarative_base()):
 	def __repr__(self):
 		return '<Task: %r @ %r>' % (self.action, self.time)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
-Session = sessionmaker()
-Session.configure(bind=engine)
-session = Session()
-t = session.query(Task).filter_by(hasRan=False).first() 
-print t
+def LoopChecker(session):
+	curTime = datetime.datetime.now()
+	tasks = session.query(Task).filter(and_(Task.hasRan == False, Task.time < curTime))
+
+	print "Printing Tasks..."
+	for task in tasks:
+		print task
+
+def start():
+	engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+	Session = sessionmaker()
+	Session.configure(bind=engine)
+	session = Session()
+	#t = session.query(Task).filter_by(hasRan=False).first() 
+
+	while True:
+		LoopChecker(session)
+		time.sleep(2)
+
+#import pdb; pdb.set_trace()
--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 #!flask/bin/python
 from app import app
+from Scheduler import Scheduler
+import threading
+
+
+t = threading.Thread(target=Scheduler.start)
+t.start()
+print "Started polling"
 app.run(debug = True, host="0.0.0.0")
+print "Started server"
